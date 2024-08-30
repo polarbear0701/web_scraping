885cf2c824ce75c8db18ca327ffab915f2bf4121
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2024-08-30 16:57:45 +0700

    integrate extracting news feature into server

--- news_server.py
+++ news_server.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, send_file, request
 import os
+from automate_news import extract_news_list
+from multiprocessing import Process
+import sched
+import time
+import json
+
 
 app = Flask(__name__)
+
+with open('websites.json', 'r') as json_file:
+    list_of_websites = json.load(json_file)
 
 def construct_csv_path() -> list:
     csv_folder = 'csv_folder'
@@ -11,7 +20,9 @@ def construct_csv_path() -> list:
             csv_files.append(os.path.join(csv_folder, csv_file))
     return csv_files
         
-        
+
+def update_news_list():
+    extract_news_list(list_of_website=list_of_websites)
         
 
 @app.route('/')
@@ -32,5 +43,16 @@ def download_file():
     else:
         return "File not found", 404
 
+def start_server():
+    app.run(debug=True, host='0.0.0.0', port=12345)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    p1 = Process(target=extract_news_list(list_of_website=list_of_websites))
+    p2 = Process(target=app.run(debug=True, host='0.0.0.0', port=12345))
+    # app.run(debug=True, host='0.0.0.0', port=12345)
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
